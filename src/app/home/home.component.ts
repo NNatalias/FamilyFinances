24bdc947eeaeda96b7f0923e9c6e2497febfa39e
vafinalex@gmail.com <vafin1994>
2020-10-10 03:16:46 +0300

    update purchases list without page reload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Purchase} from '../Interfaces/Purchase';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Category} from '../Interfaces/category';
@@ -13,10 +13,11 @@ import {AllPurchaseService} from '../services/allPurchase.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor( private newPurchaseRegistrationService: NewPurchaseService,
-               private snackBar: MatSnackBar,
-               private auth: AuthService,
-               private allPurchaseService: AllPurchaseService) { }
+  constructor(private newPurchaseRegistrationService: NewPurchaseService,
+              private snackBar: MatSnackBar,
+              private auth: AuthService,
+              private allPurchaseService: AllPurchaseService) {
+  }
 
   form: FormGroup;
   categories: Category[] = [
@@ -31,48 +32,49 @@ export class HomeComponent implements OnInit {
       sumItem: new FormControl(null, Validators.required),
       newCategory: new FormControl(null, Validators.max(20)),
       currentCategory: new FormControl('', Validators.required),
-      newProducts: new FormArray([], )
+      newProducts: new FormArray([])
     });
   }
 
-  submit(): void{
-    if ( this.form.invalid){
+  submit(): void {
+    if (this.form.invalid) {
       return;
     }
     const registration: Purchase =
-     {
-       owner: this.auth.userId,
-       nameOfShop: this.form.value.nameShop,
-       sum: this.form.value.sumItem,
-       category: this.form.value.currentCategory.value,
-       products: this.form.value.newProducts,
-       date: new Date(),
-     };
+      {
+        owner: this.auth.userId,
+        nameOfShop: this.form.value.nameShop,
+        sum: this.form.value.sumItem,
+        category: this.form.value.currentCategory.value,
+        products: this.form.value.newProducts,
+        date: new Date(),
+      };
     this.newPurchaseRegistrationService.create(registration).subscribe(() => {
       this.form.reset();
       this.openSnackBar('Новая запись успешно добавлена');
-      window.location.reload();
-      this.allPurchaseService.getAll();
+      this.allPurchaseService.getAll().subscribe((purchases: any) =>
+        this.allPurchaseService.purchases = purchases
+      );
     }, error => {
       this.form.reset();
       this.openSnackBar('Ошибка сервера!');
-    } );
+    });
   }
 
-  addNewCategory(): void{
+  addNewCategory(): void {
     const newCategory = this.form.get('newCategory').value;
     this.categories.push({value: newCategory});
     this.form.get('newCategory').reset();
     this.openSnackBar('Новая категория успешно добавлена');
-}
+  }
 
-  openSnackBar(message: string): void{
+  openSnackBar(message: string): void {
     this.snackBar.open(message, '', {
       duration: 2000,
     });
   }
 
-  addProduct(): void{
+  addProduct(): void {
     const control = new FormControl('', Validators.required);
     (this.form.controls.newProducts as FormArray).push(control);
   }
